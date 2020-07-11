26858c911fee7640ae0b52c00ccc13b6acf10a6f
ZhaoGuan <guanzhaogz@gmail.com>
2020-07-11 16:55:58 +0800

    gitlab ci

--- sonar_result.sh
+++ sonar_result.sh
@@ -1,7 +1,9 @@
 sonar_host=$1
 projectKey=$2
 sleep 10
-result=$(curl "${sonar_host}"/api/qualitygates/project_status\?projectKey\="${projectKey}" | grep -Eo '"projectStatus":{+"status":"OK"' | grep -c "OK")
+response=$(curl "${sonar_host}"/api/qualitygates/project_status\?projectKey\="${projectKey}")
+echo "${response}"
+result=$(echo "${response}" | grep -Eo '"projectStatus":{"status":"OK"' | grep -c "OK")
 if [ "${result}" == 1 ]; then
   echo "${result}"
 else
