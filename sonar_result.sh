0557afa3f71e712611d3345195cf23296fd44f55
ZhaoGuan <guanzhaogz@gmail.com>
2020-07-11 16:50:47 +0800

    gitlab ci

--- sonar_result.sh
+++ sonar_result.sh
@@ -1,5 +1,6 @@
 sonar_host=$1
 projectKey=$2
+sleep 10
 result=$(curl "${sonar_host}"/api/qualitygates/project_status\?projectKey\="${projectKey}" | grep -Eo '"projectStatus":{+"status":"OK"' | grep -c "OK")
 if [ "${result}" == 1 ]; then
   echo "${result}"
